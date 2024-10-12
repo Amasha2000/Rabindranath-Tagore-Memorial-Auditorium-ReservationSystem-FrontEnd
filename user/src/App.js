cbd1f73233f96adf020cbdaf1d6eca7ffa7373fa
Amasha <amashagalhenage@gmail.com>
2024-10-12 13:00:12 +0530

    Content has updated with components

---
+++ user/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+import PaymentWorkflow from './components/PaymentWorkFlow/PaymentWorkflow'
+import Payment from './components/PaymentWorkFlow/Payment'; 
+import SignIn from './components/Auth/SignIn'
+import SignUp from './components/Auth/SignUp'
+import Form from './components/Form/SubmissionForm'     
+import Notification from './components/Notification/Notification'  
+import AvailabilityCalendar from './components/Calendar/AvailabilityCalendar';  
+
+const stripePromise = loadStripe('pk_test_51NTgKRAaWvD0FBmzmVquePz9uFILsjLUYey7pNiY4zIiKVyuzhYAPMdWg7USuiUujmsRvNUC1H14x666RiZKa53g00xLyoEIOj');
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/cal" element={<AvailabilityCalendar />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/payment" element={<PaymentWorkflow />} />
+        <Route path="/noti" element={<Notification />} /> 
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route
+          path="/payment/:reservationId/:amount/:paymentType"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
